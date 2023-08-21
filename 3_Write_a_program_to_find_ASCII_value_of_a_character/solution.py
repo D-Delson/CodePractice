d3b92dde8bb04fd7ed4a02efc10c7eb9d4f89d21
D-Delson <delson150203@gmail.com>
2023-08-21 18:30:26 +0530

    Added problem_statement.txt

---
+++ 3_Write_a_program_to_find_ASCII_value_of_a_character/solution.py
@@ -0,0 +1,34 @@
+def find_ascii(character: str) -> int:
+    """Return the ASCII value of a character."""
+    return ord(character)#ord() returns the ASCII value
+
+if __name__ == "__main__":
+    char = input("Enter a single character: ")
+
+    # Check if the input is a single character
+    if len(char) == 1:
+        ascii_value = find_ascii(char)
+        print(f"The ASCII value of '{char}' is {ascii_value}.")
+    else:
+        print("Please enter only a single character.")
+def find_ascii(character: str) -> int:
+    """
+    Return the ASCII value of a character.
+
+    Parameters:
+    - character (str): A single character for which the ASCII value is to be found.
+
+    Returns:
+    - int: The ASCII value of the given character.
+    """
+    return ord(character)  # ord() returns the ASCII value
+
+if __name__ == "__main__":
+    char = input("Enter a single character: ")
+
+    # Check if the input is a single character
+    if len(char) == 1:
+        ascii_value = find_ascii(char)
+        print(f"The ASCII value of '{char}' is {ascii_value}.")
+    else:
+        print("Please enter only a single character.")
