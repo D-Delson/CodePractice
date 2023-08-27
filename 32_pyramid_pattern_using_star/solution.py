5379010143d37d48e13b31f030d8c6a94fe8e4e6
D-Delson <delson150203@gmail.com>
2023-08-27 23:39:43 +0530

    Added problem statement and solution.py

---
+++ 32_pyramid_pattern_using_star/solution.py
@@ -0,0 +1,13 @@
+def pyramid(rows):
+    for i in range(rows):
+        #print spaces
+        for _ in range(rows-i-1):
+            print(' ', end = '')
+        #print star
+        for _ in range(2*i+1):
+            print('*', end='')
+        print()
+
+if __name__ == '__main__':
+    rows = int(input('Enter depth of the pyramid: '))
+    pyramid(rows)
