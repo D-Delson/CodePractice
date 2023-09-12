4ab745170a3ad1e2f295d538869deb458c95b89d
D-Delson <delson150203@gmail.com>
2023-09-12 21:44:29 +0530

    Updated the solution

--- 77_binary_divisible/solution.py
+++ 77_binary_divisible/solution.py
@@ -1,7 +1,16 @@
-values = input('Enter the four digit binary values(comma separeted): ').split(',')
-divisible_by_5 = []
-for binary in values:
-    tot = sum([int(bit) * (2**idx) for idx, bit in enumerate(list(binary)[::-1])])
-    if tot % 5 == 0:
-        divisible_by_5.append(binary)
-print(*divisible_by_5, sep=',', end=' ')
+# values = input('Enter the four digit binary values(comma separeted): ').split(',')
+# divisible_by_5 = []
+# for binary in values:
+#     tot = sum([int(bit) * (2**idx) for idx, bit in enumerate(list(binary)[::-1])])
+#     if tot % 5 == 0:
+#         divisible_by_5.append(binary)
+# print(*divisible_by_5, sep=',', end=' ')
+
+
+values = []
+items = [x for x in input().split(',')]
+for p in items:
+    intp = int(p, 2)
+    if not intp % 5:
+        values.append(p)
+print(','.join(values))
